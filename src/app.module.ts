4ddcecb2b2c9628a58716298f16fef7ef09f102b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-04-30 18:51:25 -0300

    feat: configure global environment variables and TypeORM in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod', 'staging', 'test')
+          .required(),
+        PORT: Joi.number().optional(),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
