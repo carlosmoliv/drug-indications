5c6da15a401e22ccd4db7844e19fb741e3c5b293
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-05-01 01:08:07 -0300

    feat(auth): integrate JwtModule and add TokenService to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenService } from './services/token.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, TokenService],
   controllers: [AuthController],
 })
 export class AuthModule {}
