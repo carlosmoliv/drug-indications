5e764fec3ab009f0aedf4d48fd3e1974b1517f8a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-05-10 15:10:34 -0300

    feat: add IndicationsModule

---
+++ src/indications/indications.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { DailyMedService } from './application/services/dailymed/dailymed.service';
+import { XmlParserService } from './infra/services/xml/xml-parser.service';
+import { DailymedController } from './presentation/controllers/dailymed.controller';
+import { DailyMedClient } from './infra/services/dailymed/dailymed-client.service';
+import { GoogleGenAIService } from './infra/services/ai/google-gen-ai.service';
+import { Icd10Service } from './application/services/icd10/icd10.service';
+
+@Module({
+  providers: [
+    DailyMedService,
+    XmlParserService,
+    DailyMedClient,
+    GoogleGenAIService,
+    Icd10Service,
+  ],
+  controllers: [DailymedController],
+})
+export class IndicationsModule {}
