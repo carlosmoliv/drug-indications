382e36cc765a8628a3fcd142e3cee9f764087c37
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-05-10 16:55:30 -0300

    feat: add ICD10_DATA provider using imported ICD10 codes

--- src/indications/indications.module.ts
+++ src/indications/indications.module.ts
@@ -5,6 +5,7 @@ import { DailymedController } from './presentation/controllers/dailymed.controll
 import { DailyMedClient } from './infra/services/dailymed/dailymed-client.service';
 import { GoogleGenAIService } from './infra/services/ai/google-gen-ai.service';
 import { Icd10Service } from './application/services/icd10/icd10.service';
+import icd10DataImport from '../data/codes_icd10.json';
 
 @Module({
   providers: [
@@ -13,6 +14,10 @@ import { Icd10Service } from './application/services/icd10/icd10.service';
     DailyMedClient,
     GoogleGenAIService,
     Icd10Service,
+    {
+      provide: 'ICD10_DATA',
+      useValue: icd10DataImport,
+    },
   ],
   controllers: [DailymedController],
 })
